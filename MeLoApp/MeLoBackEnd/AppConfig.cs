9f7a6e605235df6ba7fc67b235c9f57f83a873b5
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-12-13 10:29:17 +0100

    Implement AppConfig class for storing user configurations

--- MeLoApp/MeLoBackEnd/AppConfig.cs
+++ MeLoApp/MeLoBackEnd/AppConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,45 @@ using System.Threading.Tasks;
 
 namespace MeLoBackEnd
 {
+    [Serializable]
     class AppConfig
     {
+        private string preferredPath;
+
+        public string PreferredPath
+        {
+            get { return preferredPath; }
+            set { preferredPath = value; }
+        }
+
+        private List<OpenFileDialog> recentlyOpenedFiles = new List<OpenFileDialog>();
+        
+        public List<OpenFileDialog> RecentlyOpenedFiles
+        {
+            get { return recentlyOpenedFiles = new List<OpenFileDialog>(); }
+            set { recentlyOpenedFiles = value; }
+        }
+
+        private List<string> recentFilePaths = new List<string>();
+
+        public List<string> RecentFilePaths
+        {
+            get { return recentFilePaths; }
+            set { recentFilePaths = value; }
+        }
+
+
+        public AppConfig(string preferredPath, List<OpenFileDialog> recentlyOpenedFiles)
+        {
+            PreferredPath = preferredPath;
+            RecentlyOpenedFiles = recentlyOpenedFiles;
+            RecentFilePaths.Clear();
+            foreach (OpenFileDialog dialog in RecentlyOpenedFiles)
+            {
+                RecentFilePaths.Add(dialog.SafeFileName);
+            }
+        }
+
+
     }
 }
