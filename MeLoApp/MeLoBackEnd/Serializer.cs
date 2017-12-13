ff1582d8d38332e8c945cacc2888cb30159067fa
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-12-13 10:28:29 +0100

    Implement Serializer class with Serialize and Deserialize features

---
+++ MeLoApp/MeLoBackEnd/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace MeLoBackEnd
+{
+    class Serializer
+    {
+        private XmlSerializer serializer = new XmlSerializer(typeof(AppConfig));
+
+        public void SerializeConfig(AppConfig appConfig)
+        {
+            TextWriter writer = new StreamWriter("AppConfig.xml", false);
+            serializer.Serialize(writer, appConfig);
+            writer.Close();
+        }
+
+        public AppConfig DeserializeConfig()
+        {
+            TextReader reader = new StreamReader("AppConfig.xml");
+            AppConfig appConfig = (AppConfig)serializer.Deserialize(reader);
+            reader.Close();
+
+            return appConfig;
+        }
+    }
+}
