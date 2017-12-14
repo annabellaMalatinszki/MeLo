dfd7db9e20436b124780190821a1b8e03f79d04e
zsoltiNagy <zsoltiusz.nagy@gmail.com>
2017-12-14 14:53:42 +0100

    Wrote two tests to tests if the testing framework is even able to test.

---
+++ MeLoApp/UnitTestTestTest/GUISupportTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using WPFMediaPlayer;
+namespace MeLoGUI.Test
+{ 
+    [TestFixture]
+    public class GUISupportTest
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            GUISupport sut = new GUISupport();
+            int expectedResult = sut.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+            GUISupport sut = new GUISupport();
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+    }
+}
